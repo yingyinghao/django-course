33362ced1ac04ab085bd9d1801676c5d564a153a
Yingying <92928939+yingyinghao@users.noreply.github.com>
2025-03-22 23:30:38 -0400

    lesson02

--- lesson02/myproject/myproject/urls.py
+++ lesson02/myproject/myproject/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/', views.about),
-    path('posts/', include('posts.urls'))
+    path('posts/',include('posts.urls'))
 ]
